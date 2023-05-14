a40c6a1de5bb3b7b32b8f167da339ea92674509b
Roman Klimov <timeflieswhenyouhf@internet.ru>
2023-05-14 17:31:53 +0300

    first

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "./index.css";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <Counter />
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { shuffle } from "fast-shuffle";
+
+  let list: Array<number> = [3, 4, 5, 6];
+  const shuffle_list = () => {
+    list = shuffle(list);
+  };
+</script>
+
+<div
+  on:keydown={shuffle_list}
+  on:click={shuffle_list}
+  class="max-w-[400px] m-auto"
+>
+  {import.meta.env.MODE}
+  {#each list as item}
+    <div class="bg-slate-900 w-10 h-10 rounded-md p-2 m-2">
+      <p class="font-semibold text-lg text-white">{item}</p>
+    </div>
+  {/each}
+</div>
